bedf89823007f3b8922014a23b7c4206b478289b
Nichi 2130 <dainichi@me.com>
2015-10-19 03:11:35 -0400

    Added WF.EMC.RID.AAF.NichiVer solution and DataLayer Project.

---
+++ WF.EMC.RID.AAF.VerNiChI/DataLayer/AccessDataLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//todo<!-- 1 add using statements -->
+using System.Data;
+using System.Data.SqlClient;
+//todo<!-- 1 end -->
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    /// <summary>
+    /// DD-- AccessDataLayer handles the interactions with the database
+    /// </summary>
+    public class AccessDataLayer
+    {
+    }
+}
